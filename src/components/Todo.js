f5419fd17bf8a6206198aef2459a310e336b94c5
Daniel-Toplian <danieltoplian@gmail.com>
2023-07-08 20:42:55 +0300

    add classNames to Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
- 
+export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
   return (
-    <div className="Todo">
-        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
-        </div>
+    <div className='Todo'>
+      <p
+        className={`${task.completed ? 'completed' : ''}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.task}
+      </p>
+      <div className='todo-options'>
+        <FontAwesomeIcon
+          className='edit-btn'
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className='delete-btn'
+          icon={faTrash}
+          onClick={() => deleteTodo(task.id)}
+        />
+      </div>
     </div>
   )
 }
